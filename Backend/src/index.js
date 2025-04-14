161402a7c5000af0370ec08869b12fcb5cfdba77
Jeronimo Fabian Albusto <jf1230@enterprise1.com>
2025-04-14 12:06:37 -0300

    se modifico el index.js del back

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+//const bcrypt = require('bcryptjs');
+//const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
 
